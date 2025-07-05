5f7780b87cdb812aa04d68ea1d1dc1b39c3af241
Lajide <lajide@Lajides-MacBook-Pro.local>
2025-07-05 14:57:20 +0100

    Create a new sales register with payment status

---
+++ viewsight/viewsight/report/party_sales_register/party_sales_register.js
@@ -0,0 +1,97 @@
+// Copyright (c) 2025, Jide (lajidey) and other Oasyx Developers and contributors
+// For license information, please see license.txt
+
+const status_colors = {
+			Draft: "red",
+			Unpaid: "orange",
+			Paid: "green",
+			Return: "gray",
+			"Credit Note Issued": "gray",
+			"Unpaid and Discounted": "orange",
+			"Partly Paid and Discounted": "yellow",
+			"Overdue and Discounted": "red",
+			Overdue: "red",
+			"Partly Paid": "yellow",
+			"Internal Transfer": "darkgrey",
+		};
+
+frappe.query_reports["Party Sales Register"] = {
+	filters: [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			width: "80",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+		},
+		{
+			fieldname: "customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options: "Customer",
+		},
+		{
+			fieldname: "customer_group",
+			label: __("Customer Group"),
+			fieldtype: "Link",
+			options: "Customer Group",
+		},
+		{
+			fieldname: "owner",
+			label: __("Owner"),
+			fieldtype: "Link",
+			options: "User",
+		},
+		{
+			fieldname: "cost_center",
+			label: __("Cost Center"),
+			fieldtype: "Link",
+			options: "Cost Center",
+		},
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+		},
+	],
+	collapsible_filters: true,
+	seperate_check_filters: true,
+	formatter: function (value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (column.fieldname == "out_qty" && data && data.out_qty < 0) {
+			value = "<span style='color:#ed2424'><b>" + value + "</b></span>";
+		} else if (column.fieldname == "in_qty" && data && data.in_qty > 0) {
+			value = "<span style='color:green'><b>" + value + "</b></span>";
+		}
+
+		try {
+			if(column.fieldname == 'status'){
+				//console.log('12     3 :'+ data.status);
+				value = `<span class='indicator-pill ${status_colors[data.status]} filterable no-indicator-dot ellipsis'> ${value}</span>`
+			}
+		} catch (err) {
+			console.log('invalid')
+			console.log(err)
+		}
+
+		return value;
+	},
+
+};
+
+erpnext.utils.add_dimensions("Sales Register", 7);
+
+/* function(i,e){
+	<span class="indicator-pill green filterable no-indicator-dot ellipsis" ></span>
+		element = `<span class='indicator-pill ${status_colors[doc.status]} filterable no-indicator-dot ellipsis'`;
+} 
+//status_colors[doc.status]
+*/
